ff6157a32b4e857eda0f6ed26114f4c70a7f38ab
Erdal Budak <erdalbudak89@gmail.com>
2022-03-08 23:07:23 -0500

    commit tree 60a44535f3ab441a2d42df1de40713addc9ec746

--- test/index-test.js
+++ test/index-test.js
@@ -1,12 +1,10 @@
 const { name, height, message } = require("../index.js");
 
-/*
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
-    })
-  })
+describe("Name", () => {
+  it('returns "Joe"', () => {
+    expect(name).toEqual("Joe");
+  });
+});
 
 
   describe('Height', () => {
@@ -21,5 +19,5 @@ describe('what-is-a-test', () => {
       expect(message).toInclude(height)
     })
   })
-})
-*/
+
+
